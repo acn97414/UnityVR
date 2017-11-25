f6d8f4b55fc28250ddeda92773ba7a4ad4ecb74d
4419-2-3\user <tree50570@gmail.com>
2017-11-25 14:45:43 +0800

    7.待下次繼續完成

--- UnityVR-Startup/Assets/Scripts/ShootingTarget.cs
+++ UnityVR-Startup/Assets/Scripts/ShootingTarget.cs
@@ -9,6 +9,7 @@ public class ShootingTarget : MonoBehaviour
 {
     public int score = 1;
     public float destroyTimeOutDuration = 2f;
+    public float timeOutDuration = 2f;
     public event Action<ShootingTarget> OnRemove;
     private Transform cameraTransfrom;
     private AudioSource audioSource;
@@ -17,7 +18,11 @@ public class ShootingTarget : MonoBehaviour
     private Collider mCollider;
     public AudioClip destroyClip;
     public GameObject destroyPrefab;
+    public AudioClip spawnClip;
+    public AudioClip missedClip;
+
     private bool isEnding;
+
 
     private void Awake()
     {
@@ -46,6 +51,8 @@ public class ShootingTarget : MonoBehaviour
     }
     private IEnumerator OnHit()
     {
+        if (isEnding)
+            yield break;
         mRenderer.enabled = false;
         mCollider.enabled = false;
         audioSource.clip = destroyClip;
@@ -54,6 +61,31 @@ public class ShootingTarget : MonoBehaviour
         GameObject destroyedTarget = Instantiate<GameObject>(destroyPrefab, transform.position, transform.rotation);
         Destroy(destroyedTarget, destroyTimeOutDuration);
         yield return new WaitForSeconds(destroyClip.length);
+        if (OnRemove != null)
+            OnRemove(this);
+    }
+
+    public void Restart()
+    {
+        mRenderer.enabled = true;
+        mCollider.enabled = true;
+        isEnding = false;
+        audioSource.clip = spawnClip;
+        audioSource.Play();
+        transform.LookAt(cameraTransfrom.position);
+        StartCoroutine(MissTarget());
+    }
+
+    private IEnumerator MissTarget()
+    {
+        yield return new WaitForSeconds(timeOutDuration);
+        if (isEnding)
+            yield break;
+        isEnding = true;
+        mRenderer.enabled = false;
+        mCollider.enabled = missedClip;
+        audioSource.Play();
+        yield return new WaitForSeconds(missedClip.length);
         if (OnRemove != null)
             OnRemove(this);
     }
